ff23f69100b2e4f8832ceb68dcf3d3cfa3170a02
Cleber-victor <cvsj@cin.ufpe.br>
2026-04-02 12:15:10 -0300

    add security to passwords

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-import app from "./app.js";
+import express from "express";
+import cors from "cors";
+import pool from "./src/config/database.js";
+import errorHandling from "./src/middlewares/errorHandling.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import expenseRoutes from "./src/routes/expenseRoutes.js";
+const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.listen(PORT, () => {
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", userRoutes);
+app.use("/api", expenseRoutes);
+
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.send(`The database name is: ${result.rows[0].current_database}`);
+});
+
+
+app.use(errorHandling);
+
+app.listen(PORT,() =>{
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 import pool from "../config/database.js";
+import bcrypt from "bcrypt";
 
 export const getAllUsersService = async () => {
   const result = await pool.query("SELECT * FROM users");
@@ -11,22 +12,31 @@ export const getUserByIdService = async (id) => {
 };
 
 export const createUserService = async (user) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(user.password, saltRounds);
   const result = await pool.query(
     "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
-    [user.name, user.email, user.password]
+    [user.name, user.email, hashedPassword]
   );
   return result.rows[0];
 };
 
 export const updateUserService = async (id, user) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(user.password, saltRounds);
   const result = await pool.query(
     "UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *",
-    [user.name, user.email, user.password, id]
+    [user.name, user.email, hashedPassword, id]
   );
   return result.rows[0];
 };
 
 export const deleteUserService = async (id) => {
   const result = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *", [id]);
+  return result.rows[0];
+};
+
+export const getUserByEmailService = async (email) => {
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
   return result.rows[0];
 };
